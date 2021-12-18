6b579725e4b109cdb359eb3d606ddffb5e9615db
dibakorseu46 <dibakorroy56.com>
2021-12-18 22:29:12 +0600

    Finished Project

--- Cards/src/app/app.component.ts
+++ Cards/src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Cards';
+  posts=[
+    {
+      title:'Neat Tree',
+      imageUrl:'assets/tree.jpeg',
+      userName:'nature',
+      content:'I sam the neat tree today'
+    },
+    {
+      title:'Snowy Mountain',
+      imageUrl:'assets/mountain.jpeg',
+      userName:'MountainLover',
+      content:'Here is picture of snowy mountian'
+    },
+    {
+      title:'Mountain Biking',
+      imageUrl:'assets/biking.jpeg',
+      userName:'biking1234',
+      content:'I did some biking today'
+    }
+  ];
 }
